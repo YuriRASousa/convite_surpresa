4093f18eab1b694479e24dcefd912e7772929772
YuriRASousa <152436639+YuriRASousa@users.noreply.github.com>
2024-09-23 22:38:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showPreviousDate() {
+    var previousDate = document.getElementById("previousDate");
+    previousDate.style.display = previousDate.style.display === "none" || previousDate.style.display === "" ? "block" : "none";
+  }
+  
+  function showNextDate() {
+    var nextDate = document.getElementById("nextDate");
+    nextDate.style.display = nextDate.style.display === "none" || nextDate.style.display === "" ? "block" : "none";
+  }
+  
